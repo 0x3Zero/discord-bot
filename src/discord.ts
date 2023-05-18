6daf6b8c02214ccc97277fe40a28ca1b04167726
zulhusnihariz <zulhusnihariz97@gmail.com>
2023-05-18 14:29:49 +0800

    add new channels

--- src/discord.ts
+++ src/discord.ts
@@ -14,7 +14,7 @@ type Transaction = Pick<
 
 const discord = new Client({ intents: GatewayIntentBits.Guilds });
 
-const CHANNELS_TO_SEND_MESSAGE = ['first-bot-testroom'];
+const CHANNELS_TO_SEND_MESSAGE = ['first-bot-testroom', 'new-beats'];
 
 discord.on(`${CustomEvents.NewTransactions}`, async (tx: Transaction[]) => {
 	CHANNELS_TO_SEND_MESSAGE.forEach((name) => {
@@ -27,11 +27,20 @@ discord.on(`${CustomEvents.NewTransactions}`, async (tx: Transaction[]) => {
 
 		const channel = discord.channels.cache.get(found.id);
 
-		let unique = tx.filter((t) => t.token_id);
+		let unique: Transaction[] = tx.filter(
+			(obj: Transaction, index: number, self: Transaction[]) =>
+				index === self.findIndex((t) => t.token_id === obj.token_id)
+		);
 
 		unique.forEach((tx) => {
 			//@ts-ignore
 			channel.send(`New beat has been added to Collabeat #${tx.token_id}`);
+
+			// remixed
+			// channel.send(`Sheet ${tx.token_id} has been remixed from Sheet ${tx.token_id} by ${tx.public_key} `)
+
+			// bookmarked
+			// channel.send(`Sheet ${tx.token_id} has been bookmarked by ${tx.public_key} `)
 		});
 	});
 });
